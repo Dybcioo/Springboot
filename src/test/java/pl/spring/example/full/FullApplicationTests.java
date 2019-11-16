b745821416ef6823420c769e968ad30706597793
Dominik <dybowski.dominik@o2.pl>
2019-11-16 10:02:19 +0100

    first spring commit

---
+++ src/test/java/pl/spring/example/full/FullApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.spring.example.full;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
