0bad992b1c55920759c1b4d04a177db5e81b3ada
Dominik <dybowski.dominik@o2.pl>
2019-11-16 13:16:52 +0100

    someee

--- src/main/java/pl/spring/example/full/FullApplication.java
+++ src/main/java/pl/spring/example/full/FullApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FullApplication {
-
+    //this method runs Spring container
     public static void main(String[] args) {
         SpringApplication.run(FullApplication.class, args);
     }
